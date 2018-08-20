af76ca88b0689bfefd68dfa108355c93b14fbeec
aaron <carteraaron144@gmail.com>
2018-08-20 15:50:47 -0500

    Added new styles to projects

--- vuetest.js
+++ vuetest.js
@@ -7,13 +7,29 @@ var app = new Vue({
 
 var test = new Vue({
   el: '#projects',
-  data: [
-    {
-      title: "DataSpace"
-    },
-    {
-      title: ""
-    }
-  ]
+  data: {
+    projects: [
+      {
+        title: "Project A",
+        imageUrl: "",
+        description: "This one took a lot of my life."
+      },
+      {
+        title: "Project B",
+        imageUrl: "",
+        description: "This one took a relatively small portion of my life."
+      },
+      {
+        title: "Project C",
+        imageUrl: "",
+        description: "This one has some of my favorite features."
+      },
+      {
+        title: "Project D",
+        imageUrl: "",
+        description: "I drove this one to the railroad station and gave it a ticket. It thanked me and waved as it drifted over the horizon."
+      }
+    ]
+  }
 })
 
